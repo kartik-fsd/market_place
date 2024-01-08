3aa21cda3ff6d722ed2e608f22ecd29ae5b2a500
dev kartik <1kartikshettar@gmail.com>
2024-01-09 00:20:46 +0530

    Market place set up is created

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+export const PRODUCT_CATEGORIES = [
+    {
+      label: "UI Kits",
+      value: "UI Kits" as const,
+      featured: [
+        {
+          name: "Editor's Choices",
+          href: "#",
+          imageSrc: "/nav/ui-kits/blue.jpg",
+        },
+        {
+          name: "New Arrivals",
+          href: "#",
+          imageSrc: "/nav/ui-kits/mixed.jpg",
+        },
+        {
+          name: "Top Sellers",
+          href: "#",
+          imageSrc: "/nav/ui-kits/purple.jpg",
+        },
+      ],
+    },
+    {
+      label: "Icons",
+      value: "icons" as const,
+      featured: [
+        {
+          name: "Editor's Picks",
+          href: "#",
+          imageSrc: "/nav/icons/picks.jpg",
+        },
+        {
+          name: "Latest Additions",
+          href: "#",
+          imageSrc: "/nav/icons/new.jpg",
+        },
+        {
+          name: "Popular Icons",
+          href: "#",
+          imageSrc: "/nav/icons/bestsellers.jpg",
+        },
+      ],
+    },
+  ];
+  
