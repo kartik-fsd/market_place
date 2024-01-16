c2ce022ec22ae2685752a36bc49a5affe1ebb9d5
dev kartik <1kartikshettar@gmail.com>
2024-01-16 20:31:24 +0530

    import change revert

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -17,7 +17,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from '@/lib/validators/account-credentials-validator'
-import { trpc } from '../../../trpc/client'
+import { trpc } from '@/trpc/client'
 import { toast } from 'sonner'
 import { ZodError } from 'zod'
 import { useRouter, useSearchParams } from 'next/navigation'
--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { trpc } from '../trpc/client'
+import { trpc } from '@/trpc/client'
 import { Loader2, XCircle } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
